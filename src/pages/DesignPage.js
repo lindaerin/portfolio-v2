349350caea71de215471bf8c33f86b4898dac3be
lindaerin <43620087+lindaerin@users.noreply.github.com>
2021-11-01 15:35:52 -0400

    add about, design, work pages

---
+++ src/pages/DesignPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Design from "../components/UIDesign/index";
+
+const DesignPage = () => {
+    return (
+        <>
+            <Design />
+        </>
+    )
+};
+
+export default DesignPage
