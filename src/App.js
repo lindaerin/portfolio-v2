d675e356f8c22611892a733de882d9f2574016fb
lindaerin <43620087+lindaerin@users.noreply.github.com>
2021-11-01 14:05:37 -0400

    update components in route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+import Sidebar from "./components/Sidebar";
+import HomePage from "./pages/HomePage";
+import Footer from "./components/Footer";
 
 import "../src/index.css";
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <Router>
-      {/* <Sidebar isOpen={isOpen} toggle={toggle} /> */}
-      <Navbar />
-      <Hero />
-      {/* <ScrollTop />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      {/* <ScrollTop /> */}
       <Switch>
         <Route path="/" component={HomePage} exact />
-        <Route path="/about" component={AboutPage} exact />
+        {/* <Route path="/about" component={AboutPage} exact />
         <Route path="/work" component={WorkPage} exact />
-        <Route path="/design" component={DesignPage} exact />
+        <Route path="/design" component={DesignPage} exact /> */}
       </Switch>
-      
-      <Footer /> */}
+      <Footer />
     </Router>
   );
 }
