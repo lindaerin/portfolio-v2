829149016861cc65f035cf9e0404da498899e70a
lindaerin <43620087+lindaerin@users.noreply.github.com>
2021-11-01 12:35:16 -0400

    add navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+// import { Mailto } from "../../Global/GlobalElements";
+import {
+  Nav,
+  NavContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./navbar.styles.js";
+
+export default function Navbar({ toggle }) {
+  return (
+    <>
+      <Nav>
+        <NavContainer>
+          <NavLogo to="/">
+              {"Linda Li \nCode & Design"}
+          </NavLogo>
+
+          <MobileIcon onClick={toggle}>
+            <FaBars />
+          </MobileIcon>
+
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="work">Work</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="design">Designs</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            {/* <Mailto
+              email="lindaerin00@gmail.com"
+              subject=" "
+              body=" "
+            > */}
+              <NavBtnLink>
+                Contact 
+              </NavBtnLink>
+            {/* </Mailto> */}
+          </NavBtn>
+        </NavContainer>
+      </Nav>
+    </>
+  );
+}
